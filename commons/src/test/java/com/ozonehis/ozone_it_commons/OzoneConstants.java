e0e5648d4d119676193e5dd569b633684a06b1e1
corneliouzbett <kipchubett@gmail.com>
2025-05-08 18:08:11 +0300

    Implement Ozone runner

---
+++ commons/src/test/java/com/ozonehis/ozone_it_commons/OzoneConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.ozone_it_commons;
+
+public class OzoneConstants {
+    
+    public static final String OZONE_PATH = "target/ozone";
+    
+    public static final String TEST_DIR_NAME = "target/ozone-test-workspace";
+    
+    public static final int DEFAULT_STARTUP_TIMEOUT_MINUTES = 10;
+}
