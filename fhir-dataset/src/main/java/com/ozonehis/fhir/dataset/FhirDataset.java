7dbac2e4ffcf07cae2686c58d744b6ddfc1de82b
corneliouzbett <kipchubett@gmail.com>
2025-05-11 19:29:29 +0300

    Add initial FHIR dataset module implementation
    Introduce a new Maven module for FHIR dataset management. Includes utility classes for loading FHIR resources, constants, and two sample patient data files. This lays the foundation for handling FHIR resources in integration tests.

---
+++ fhir-dataset/src/main/java/com/ozonehis/fhir/dataset/FhirDataset.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.fhir.dataset;
+
+import jakarta.annotation.Nonnull;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Resource;
+
+public enum FhirDataset {
+    RICHARD_JONES(getPatient("fhir/dataset/richard.patient.json")),
+
+    JOSHUA_JOHNSON(getPatient("fhir/dataset/joshua.patient.json"));
+
+    private final Resource resource;
+
+    FhirDataset(Resource resource) {
+        this.resource = resource;
+    }
+
+    static Patient getPatient(@Nonnull String filePath) {
+        return FhirDatasetUtils.loadResource(filePath, new Patient());
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+}
