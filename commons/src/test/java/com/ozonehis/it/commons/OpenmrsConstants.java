eda0dd71178e3b51549e9b9f15c2e142dc77949a
corneliouzbett <kipchubett@gmail.com>
2025-09-28 17:13:14 +0300

    Add OpenMRS constants and REST client for visit creation

---
+++ commons/src/test/java/com/ozonehis/it/commons/OpenmrsConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.it.commons;
+
+public class OpenmrsConstants {
+
+    public static final String LOCATION_OUTPATIENT = "Outpatient Clinic";
+
+    public static final String LOCATION_INPATIENT = "Inpatient Ward";
+}
